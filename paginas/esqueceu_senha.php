b54e8b965c246c891c72c0129d31cb2c1e941c2b
Athosespinner9 <95544072+ATHOSESPINNER9@users.noreply.github.com>
2023-11-01 16:31:13 -0300

    Update esqueceu_senha.php

--- paginas/esqueceu_senha.php
+++ paginas/esqueceu_senha.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Esqueceusenha</title>
+    <title>EsqueceuSenha</title>
     <link rel="stylesheet" href="../css/registre.css">
 </head>
 <body>
