753167a24695f3e7405ed2b82c1182f6e370c34c
Athosespinner9 <95544072+ATHOSESPINNER9@users.noreply.github.com>
2023-11-08 15:23:39 -0300

    Update login.php

--- paginas/login.php
+++ paginas/login.php
@@ -25,3 +25,30 @@
     </form>
 </body>
 </html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="../css/registre.css">
+</head>
+<body>
+<form action="../php/processar_login.php" method="POST" class="form">
+        <p class="title">Login</p>
+        <label>
+            <input required="" placeholder="" type="email" name="email" class="input">
+            <span>Email</span>
+        </label>
+
+        <label>
+            <input required="" placeholder="" type="password" name="password" class="input">
+            <span>Senha</span>
+        </label>
+        <p class="signin"><a href="esqueceu_senha.php">Esqueceu sua senha?</a></p>
+
+        <button type="submit" class="submit">Login</button>
+        <p class="signin">Ainda não tem cadastro? <a href="cadastro.php">Cadatre-se</a></p>
+    </form>
+</body>
+</html>
