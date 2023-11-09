ddc01017ee5b20f5ad750025c47047f7b7ab4133
KaykyCarrilho <101289953+kaykyCP2@users.noreply.github.com>
2023-11-09 14:32:38 -0300

    Add files via upload

---
+++ paginas/altera_senha.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AlterarSenha</title>
+    <link rel="stylesheet" href="../css/registre.css">
+</head>
+<body>
+<form action="../php/processar_altera_senha.php" method="POST" class="form">
+        <p class="title">Alterar Senha</p>
+        <p>Solicitação para Redefinição de Senha</p>            
+        <label>
+            <input required="" placeholder="" type="email" class="input" name="email">
+            <span>Email</span>
+        </label> 
+        <label>
+            <input required="" placeholder="" type="password" class="input" name="password">
+            <span>Senha</span>
+        </label>
+        <label>
+            <input required="" placeholder="" type="password" class="input" name="confirm_password">
+            <span>Confirmar senha</span>
+        </label>
+        <button type="submit" class="submit">Enviar</button>
+    </form>
+</body>
+</html>
