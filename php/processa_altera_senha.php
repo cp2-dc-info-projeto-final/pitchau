94e3191b62a8666b58e586b10f1eb686d4aac214
Athosespinner9 <95544072+ATHOSESPINNER9@users.noreply.github.com>
2023-11-10 09:40:05 -0300

    Update processa_altera_senha.php

--- php/processa_altera_senha.php
+++ php/processa_altera_senha.php
@@ -1,10 +1,37 @@
 <?php
     include_once "consultas/flying_bubbles.php";
-    
-    echo "bananada"
 
+    // Conectar ao banco de dados (substitua com suas credenciais)
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "pitchau";
 
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
+    if ($conn->connect_error) {
+        die("Falha na conexão com o banco de dados: " . $conn->connect_error);
+    }
 
+    // Verifique se o formulário foi enviado
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Coletar dados do formulário
+        $password_ = $_POST["password"];
+        $confirm_password = $_POST["confirm_password"];
 
+        // Validar os dados (adicionar validações adicionais conforme necessário)
+
+        // Inserir dados na tabela Usuario
+        $sql = "INSERT INTO Usuario (senha) VALUES ('$password_')";
+
+        if ($conn->query($sql) === TRUE) {
+            header("Location: ../index.php");
+            exit;
+        } else {
+            echo "Erro ao alterar senha: " . $conn->error;
+        }
+
+        // Fechar a conexão com o banco de dados
+        $conn->close();
+    }
 ?>
