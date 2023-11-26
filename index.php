5d9ccb62e8e01cafe2feded06404ff8ab44218b5
Athosespinner9 <95544072+ATHOSESPINNER9@users.noreply.github.com>
2023-11-26 15:48:31 -0300

    Update index.php

--- index.php
+++ index.php
@@ -12,14 +12,16 @@
 <body>
 <?php
   include_once "consultas/flying_bubbles.php";
-  if (!isset($_SESSION["user_id"]) && !isset($_SESSION["is_admin"])) { //Verifica se == Usuário Logado ou == Administrador
+  /*if (!isset($_SESSION["user_id"]) && !isset($_SESSION["is_admin"])) { //Verifica se == Usuário Logado ou == Administrador
     echo "<input type='hidden' id='menulevel' value='1'/>";
-  }
+  }*/
+  echo "<input type='hidden' id='menulevel' value='1'/>";
   if (isset($_SESSION["user_id"])) { //Verifica se == Usuário Logado
+    echo "<input type='hidden' id='menulevel' value='2'/>"
     if (isset($_SESSION["is_admin"])) { //Verifica se == Administrador
       echo "<input type='hidden' id='menulevel' value='3'/>";
     }
-    else echo "<input type='hidden' id='menulevel' value='2'/>";
+  else echo "<input type='hidden' id='menulevel' value='1'/>";
   }
 
 ?>
@@ -41,13 +43,15 @@
             menulevel = document.getElementById("menulevel").value;
             var menu = '';
             if(menulevel == '1'){
-              menu = '<li><a class="dropdown-item" href="paginas/login.php">Fazer Login</a></li><li><a class="dropdown-item" href="paginas/cadastro.php">Se Cadastrar</a></li>';
+              menu = '<li><a class="dropdown-item" href="../login.php">Fazer Login</a></li>
+                  <li><a class="dropdown-item" href="../cadastro.php">Se Cadastrar</a></li>';
             }
             else if(menulevel == '2'){
-              menu = '<li><a class="dropdown-item" href="paginas/perfil.php">Perfil</a></li><li><a class="dropdown-item" href="php/logout.php">Logout</a></li>';
+              menu = '<li><a class="dropdown-item" href="../perfil.php">Perfil</a></li><li><a class="dropdown-item" href="../carrinho.php">Carrinho</a></li><li><a class="dropdown-item" href="../produtos_comprados.php">Prod Comprado</a></li><li><a class="dropdown-item" href="../logout.php">Logout</a></li>';
             }
             else if(menulevel == '3'){
-              menu = '<li><a class="dropdown-item" href="paginas/perfil.php">Perfil</a></li><li><a class="dropdown-item" href="paginas/cadastro_produto.php">Cadastrar Produto</a></li>';
+              menu = '<li><a class="dropdown-item" href="../perfil.php">Perfil</a></li><li><a class="dropdown-item" href="../cadastro_categoria.php">Criar Categoria</a></li>
+<li><a class="dropdown-item" href="../cadastro_produto.php">Criar Produto</a></li><li><a class="dropdown-item" href="../produtos_vendidos.php">Prod Vendido</a></li><li><a class="dropdown-item" href="../logout.php">Logout</a></li>';
             }
             
             document.getElementById("menu").innerHTML = menu;
@@ -64,7 +68,7 @@
   </div>
 </nav>
 <?php
-include_once "consultas/flying_bubbles.php";
+include_once "../consultas/flying_bubbles.php";
 
 $imagens = carousel($servername, $username, $password, $dbname);
 if ($imagens != null) {
@@ -95,6 +99,9 @@ if ($imagens != null) {
 ?>
 
 <div class="card_list">
+
+
+    
 <?php
 
 //
@@ -113,6 +120,8 @@ $card_produto= card_produtos($servername, $username, $password, $dbname);
       echo '<div class="card-footer">';
       echo '<span class="text-title">$' . number_format($card_produto["valor"], 2) . '</span>';
       echo '<div class="card-button">';
+      echo '<div class="prod-edit">';
+        
       echo '<svg class="svg-icon" viewBox="0 0 20 20">';
       echo '<path d="M17.72,5.011H8.026c-0.271,0-0.49,0.219-0.49,0.489c0,0.271,0.219,0.489,0.49,0.489h8.962l-1.979,4.773H6.763L4.935,5.343C4.926,5.316,4.897,5.309,4.884,5.286c-0.011-0.024,0-0.051-0.017-0.074C4.833,5.166,4.025,4.081,2.33,3.908C2.068,3.883,1.822,4.075,1.795,4.344C1.767,4.612,1.962,4.853,2.231,4.88c1.143,0.118,1.703,0.738,1.808,0.866l1.91,5.661c0.066,0.199,0.252,0.333,0.463,0.333h8.924c0.116,0,0.22-0.053,0.308-0.128c0.027-0.023,0.042-0.048,0.063-0.076c0.026-0.034,0.063-0.058,0.08-0.099l2.384-5.75c0.062-0.151,0.046-0.323-0.045-0.458C18.036,5.092,17.883,5.011,17.72,5.011z"></path>';
       echo '<path d="M8.251,12.386c-1.023,0-1.856,0.834-1.856,1.856s0.833,1.853,1.856,1.853c1.021,0,1.853-0.83,1.853-1.853S9.273,12.386,8.251,12.386z M8.251,15.116c-0.484,0-0.877-0.393-0.877-0.874c0-0.484,0.394-0.878,0.877-0.878c0.482,0,0.875,0.394,0.875,0.878C9.126,14.724,8.733,15.116,8.251,15.116z"></path>';
