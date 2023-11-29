45f7e2edfad6b27ffd5a5a7770b0f0e7737df365
Igor Santana <igorsantana1601@gmail.com>
2023-11-29 01:01:31 -0300

    css

--- paginas/cadastro.php
+++ paginas/cadastro.php
@@ -5,8 +5,8 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Documento</title>
-    <link rel="stylesheet" href="css/registre.css?v=0.9">
+    <title>Cadastro</title>
+    <link rel="stylesheet" href="../css/registre.css">
 </head>
 <body>
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
