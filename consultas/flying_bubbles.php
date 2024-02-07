a1a08145d459dacd25fc65fe5d55ed9baed75ccf
Athosespinner9 <95544072+ATHOSESPINNER9@users.noreply.github.com>
2024-02-07 16:52:04 -0300

    Add files via upload

--- consultas/flying_bubbles.php
+++ consultas/flying_bubbles.php
@@ -1,23 +1,98 @@
 <?php
-session_start();
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "pitchau";
+ob_start();
+
+function connect(){
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
 
 
-function connect($servername, $username, $password, $dbname){
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    if ($conn->connect_error) {
-        die("Falha na conexão com o banco de dados: " . $conn->connect_error);
+    // Criar conexão
+    $conn = new mysqli($servername, $username, $password);
+    $databaseName = "Pitchau";
+
+    // Verificar a existência do banco de dados
+    $query = "SHOW DATABASES LIKE '$databaseName'";
+    $result = $conn->query($query);
+
+    if ($result->num_rows > 0) {
+        $conn->close();
+    } else {
+        if ($conn->connect_error) {
+            die("Falha na conexão com o banco de dados: " . $conn->connect_error);
+        }
+    
+        // Ler o conteúdo do arquivo SQL
+        $sqlFile = file_get_contents('consultas/pitchau.sql');
+    
+        // Executar script SQL para criar banco de dados e tabelas
+        if ($conn->multi_query($sqlFile)) {
+            echo "Script SQL executado com sucesso!";
+        } else {
+            echo "Erro na execução do script SQL: " . $conn->error;
+        }
+    
+        // Fechar a conexão temporária
+        $conn->close();
+    
+
     }
+
+    // Verificar a conexão
+    
+    // Conectar ao banco de dados Pitchau após a criação
+$conn = new mysqli($servername, $username, $password, 'Pitchau');
+
+
+
+// Verificar se a tabela Categoria está vazia
+$resultCategoria = $conn->query("SELECT COUNT(*) as count FROM Categoria");
+$rowCategoria = $resultCategoria->fetch_assoc();
+
+if ($rowCategoria['count'] == 0) {
+    // Inserir dados na tabela Categoria
+    $conn->query("INSERT INTO Categoria(nome) VALUES('Sopro')");
+    $conn->query("INSERT INTO Categoria(nome) VALUES('Cordas')");
+    $conn->query("INSERT INTO Categoria(nome) VALUES('Percussão')");
+    $conn->query("INSERT INTO Categoria(nome) VALUES('Teclas')");
+}
+
+// Verificar se a tabela Produto está vazia
+$resultProduto = $conn->query("SELECT COUNT(*) as count FROM Produto");
+$rowProduto = $resultProduto->fetch_assoc();
+
+if ($rowProduto['count'] == 0) {
+    // Inserir dados na tabela Produto
+    $conn->query("INSERT INTO Produto(nome, descricao, foto, valor, categoria_id) VALUES('Violão de aço', 'Violao com cordas de aço', 'img/img_produto/imagem1.png', 1600, 1)");
+    $conn->query("INSERT INTO Produto(nome, descricao, foto, valor, categoria_id) VALUES('Violão de Nylon', 'Violão com cordas de nylon', 'img/img_produto/imagem2.png', 1400, 1)");
+    $conn->query("INSERT INTO Produto(nome, descricao, foto, valor, categoria_id) VALUES('Guitarra EletroAcustica', 'Guitarra eletroacústica com cordas de aço', 'img/img_produto/imagem3.png', 3800, 1)");
+    // Adicione mais inserções de produtos conforme necessário
+}
+
+// Verificar se a tabela Slider está vazia
+$resultSlider = $conn->query("SELECT COUNT(*) as count FROM Slider");
+$rowSlider = $resultSlider->fetch_assoc();
+
+if ($rowSlider['count'] == 0) {
+    // Inserir dados na tabela Slider
+    $conn->query("INSERT INTO Slider(url_img) VALUES('img/img_slider/slider.png')");
+}
+
+
+    // Restante do seu código para verificar e inserir dados nas tabelas
+
+    // Fechar a conexão
+    
+
     return $conn;
 }
 
-function carousel($servername, $username, $password, $dbname) {
-    $conn = connect($servername, $username, $password, $dbname);
+
+function carousel() {
+    $conn = connect();
+    
     // Consulta SQL para buscar todas as URLs de imagens da tabela "Slider"
-    $sql = "SELECT url_img FROM slider";
+    $sql = "SELECT url_img FROM Slider";
     $result = $conn->query($sql);
     $imagens=[];
     if ($result->num_rows > 0) {    
@@ -26,12 +101,13 @@ function carousel($servername, $username, $password, $dbname) {
     }
     // Fechando a conexão com o banco de dados
     $conn->close();
+    
     //Retornando variavel para carrossel
     return $imagens;
 }
 
-function card_produtos($servername, $username, $password, $dbname){
-    $conn= connect($servername, $username, $password, $dbname);
+function card_produtos(){
+    $conn= connect();
     $sql = "SELECT * FROM Produto";
     $result = $conn->query($sql);
     $card_produto= [];
@@ -46,8 +122,8 @@ function card_produtos($servername, $username, $password, $dbname){
     }
 }
 
-function recuperar_produto_por_id($servername, $username, $password, $dbname, $id_produto){
-    $conn= connect($servername, $username, $password, $dbname);
+function recuperar_produto_por_id($id_produto){
+    $conn= connect();
     $sql = "SELECT * FROM Produto where id=$id_produto";
     $result = $conn->query($sql);
     $card_produto=null;
@@ -64,8 +140,8 @@ function recuperar_produto_por_id($servername, $username, $password, $dbname, $i
     }
 }
 
-function criar_carrinho($servername, $username, $password, $dbname, $min, $max, $cod_carrinho){
-    $conn= connect($servername, $username, $password, $dbname);
+function criar_carrinho( $min, $max, $cod_carrinho){
+    $conn= connect();
     $min = 000000;
     $max = 999999;
     $cod_carrinho = rand($min, $max);
@@ -73,8 +149,8 @@ function criar_carrinho($servername, $username, $password, $dbname, $min, $max, 
 
 }
 
-function add_produto_carrinho($servername, $username, $password, $dbname, $id_produto){
-    $conn= connect($servername, $username, $password, $dbname);
+function add_produto_carrinho( $id_produto){
+    $conn= connect();
     $sql = "SELECT * FROM Produto where id=$id_produto";
     $result = $conn->query($sql);
     if ($result->num_rows > 0) {
@@ -84,26 +160,30 @@ function add_produto_carrinho($servername, $username, $password, $dbname, $id_pr
     }
 }
 
-function processar_login($servername, $username, $password, $dbname, $email, $senha){
-    $conn = connect($servername, $username, $password, $dbname);
+function processar_login($email, $senha){
+    $conn = connect();
 
-    $sql = "SELECT id, nome, email, senha, isAdmin FROM Usuario WHERE email = '$email'";
+    $sql = "SELECT id, nome, email, senha, isAdmin, foto FROM Usuario WHERE email = '$email'";
     $result = $conn->query($sql);
-
+    
     if ($result->num_rows == 1) {
         $row = $result->fetch_assoc();
-        if (password_verify($senha, $row['senha'])) {
+        if ($senha == $row['senha']) {
             $_SESSION["user_id"] = $row["id"];
+            $_SESSION["foto_path"] = $row["foto"];
             $_SESSION["email"] = $email;
             $_SESSION["username"] = $row["nome"]; // Defina o nome de usuário aqui
             $_SESSION["is_admin"] = $row["isAdmin"];
             header("Location: ../index.php"); // Redirecionar para a página do painel após o login
-            exit();
+            
         } 
         else{
             return 1;
         }
         
+    }elseif($result->num_rows ==0){
+
+        header("Location: ../paginas/login.php");
     } 
  
     
@@ -112,7 +192,7 @@ function processar_login($servername, $username, $password, $dbname, $email, $se
     }
 }
 
-function perfil($servername, $username, $password, $dbname){
+function perfil(){
 
     // Verifica a conexão do usuario
 if (isset($_SESSION["email"])) { 
@@ -125,7 +205,7 @@ if (isset($_SESSION["email"])) {
     header("Location: ../paginas/login.php");
     exit;
 }
-$conn = connect($servername, $username, $password, $dbname);
+$conn = connect();
 $sql = "SELECT nome, email FROM usuario WHERE email = ?";
 $stmt = $conn->prepare($sql);
 if (!$stmt) {
@@ -144,19 +224,38 @@ if ($result->num_rows > 0) {
 
 }
 
-function alterar_senha($servername, $username, $password, $dbname){
+function alterar_senha(){
 
 }
 
-function alterar_email($servername, $username, $password, $dbname,$email, $senha){
-    $conn= connect($servername,$username,$password,$dbname);
+function pegar_foto($user_id) {
+    $conn = connect();
+
+    // Utilizando prepared statement para evitar injeções SQL
+    $stmt = $conn->prepare("SELECT foto FROM Usuario WHERE id = ?");
+    $stmt->bind_param("i", $user_id);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows == 1) {
+        $row = $result->fetch_assoc();
+        echo $row["foto"];
+    } else {
+        // Usuário não encontrado
+        return null;
+    }
+}
+
+
+function alterar_email($email, $senha){
+    $conn= connect();
     $user_id= $_SESSION["user_id"]; //Variavel de sessão
-    $sql= "SELECT email, senha FROM usuario WHERE id= '$user_id'";
+    $sql= "SELECT email, senha FROM Usuario WHERE id= '$user_id'";
     $result=$conn->query($sql);
     if($result->num_rows > 0 ){
         $row= $result->fetch_assoc();
-        if(password_verify($senha, $row['senha'])){
-            $stmt=$conn->prepare("UPDATE usuario SET email= ? WHERE id= ?");
+        if($senha == $row['senha']){
+            $stmt=$conn->prepare("UPDATE Usuario SET email= ? WHERE id= ?");
             $stmt->bind_param("si",$email, $user_id );
             if ($stmt->execute()) {
                 header("Location: ../php/logout.php");
@@ -173,10 +272,10 @@ function alterar_email($servername, $username, $password, $dbname,$email, $senha
     }
 }
 
-function Apagar_conta($servername, $username, $password, $dbname){
+function Apagar_conta(){
     $conn= connect($servername,$username,$password,$dbname);
     $user_id= $_SESSION["user_id"]; //Variavel de sessão
-    $stmt = $conn->prepare("DELETE FROM usuario WHERE id = ?");
+    $stmt = $conn->prepare("DELETE FROM Usuario WHERE id = ?");
     $stmt->bind_param("i", $user_id);
     $result= $stmt->execute();
     $stmt->close();
@@ -184,9 +283,9 @@ function Apagar_conta($servername, $username, $password, $dbname){
     return $result;
 }
 
-function getuser($servername, $username, $password, $dbname){
-    $conn= connect($servername,$username,$password,$dbname);
-    $sql = "SELECT * FROM usuario";
+function getuser(){
+    $conn= connect();
+    $sql = "SELECT * FROM Usuario";
     $result = $conn->query($sql);
     $usuarios = [];
     // Verificar se há resultados
@@ -201,10 +300,10 @@ function getuser($servername, $username, $password, $dbname){
     return $usuarios;
 }
 
-function apagar_usuarios($servername, $username, $password, $dbname, $id){
-    $conn= connect($servername,$username,$password,$dbname);
+function apagar_usuarios($id){
+    $conn= connect();
     $sql = $conn->prepare("DELETE FROM usuario WHERE id = ?");
-    $sql->bind_param("i", $id);
+    
     if ($sql->execute()) {
         header("Location: ../paginas/visualizacaoUser.php");
     } else {
@@ -214,8 +313,8 @@ function apagar_usuarios($servername, $username, $password, $dbname, $id){
     $sql->close();
 }
 
-function editName_usuarios($servername, $username, $password, $dbname, $id,$newName){
-    $conn= connect($servername,$username,$password,$dbname);
+function editName_usuarios( $id,$newName){
+    $conn= connect();
     $sql = $conn->prepare("UPDATE usuario SET nome = ? WHERE id = ?");
     $sql->bind_param("si", $newName, $id);
     if ($sql->execute()) {
@@ -227,8 +326,8 @@ function editName_usuarios($servername, $username, $password, $dbname, $id,$newN
     $sql->close();
     }
 
-function transform_admin($servername, $username, $password, $dbname,$id){
-    $conn= connect($servername,$username,$password,$dbname);
+function transform_admin($id){
+    $conn= connect();
     $sql = $conn->prepare("UPDATE usuario set isAdmin = 1 WHERE id = ?");
     $sql->bind_param("i", $id);
     if ($sql->execute()) {
@@ -240,4 +339,280 @@ function transform_admin($servername, $username, $password, $dbname,$id){
     $sql->close();
 
 }
+
+function insertIntoCarrinho($id_produto, $id_cliente, $quantidade) {
+    // Substitua 'sua_tabela' pelo nome real da tabela no seu banco de dados
+    $conn = connect();
+    
+    // Prepara a consulta SQL usando placeholders para prevenção de SQL injection
+    $sql = $conn->prepare("INSERT INTO carrinho (id_produto, id_cliente, quantidade) VALUES (?, ?, ?)");
+    
+    // Associa os parâmetros aos placeholders e define os tipos de dados
+    $sql->bind_param("iii", $id_produto, $id_cliente, $quantidade);
+    
+    // Executa a consulta SQL
+    if ($sql->execute()) {
+        header("Location: ../index.php");
+        exit();
+    } else {
+        echo "Erro ao inserir no carrinho: " . $sql->error;
+        return 1;
+    }
+    
+    // Fecha a declaração SQL e a conexão
+    $sql->close();
+    $conn->close();
+}
+function removeFromCarrinho($id_produto) {
+    $id_cliente=$_SESSION['user_id'];
+    // Substitua 'sua_tabela' pelo nome real da tabela no seu banco de dados
+    $conn = connect();
+
+    // Prepara a consulta SQL usando placeholders para prevenção de SQL injection
+    $sql = $conn->prepare("DELETE FROM carrinho WHERE id_produto = ? AND id_cliente = ? LIMIT 1");
+
+    // Associa os parâmetros aos placeholders e define os tipos de dados
+    $sql->bind_param("ii", $id_produto, $id_cliente);
+
+    // Executa a consulta SQL
+    if ($sql->execute()) {
+        header("Location: ../index.php");
+        exit();
+    } else {
+        echo "Erro ao remover do carrinho: " . $sql->error;
+        return 1;
+    }
+
+    // Fecha a declaração SQL e a conexão
+    $sql->close();
+    $conn->close();
+}
+
+
+
+function getProdutosNoCarrinhoPorCliente($id_cliente) {
+    $conn = connect();
+
+    // Substitua 'sua_tabela_carrinho' pelo nome real da tabela carrinho no seu banco de dados
+    $sql = $conn->prepare("SELECT id_produto, quantidade FROM carrinho WHERE id_cliente = ?");
+    $sql->bind_param("i", $id_cliente);
+
+    $sql->execute();
+    $result = $sql->get_result();
+
+    // Obter todos os resultados como um array associativo
+    $produtos_no_carrinho = $result->fetch_all(MYSQLI_ASSOC);
+
+    $sql->close();
+    $conn->close();
+
+    return $produtos_no_carrinho;
+}
+
+function getProdutoPorId($id_produto) {
+    $conn = connect();
+
+    // Substitua 'sua_tabela_produto' pelo nome real da tabela Produto no seu banco de dados
+    $sql = $conn->prepare("SELECT * FROM Produto WHERE id = ?");
+    $sql->bind_param("i", $id_produto);
+
+    $sql->execute();
+    $result = $sql->get_result();
+
+    // Obter o resultado como um array associativo
+    $produto = $result->fetch_assoc();
+
+    $sql->close();
+    $conn->close();
+
+    return $produto;
+}
+
+
+
+
+
+
+
+// flying_bubbles.php
+
+if (isset($_GET['method'])) {
+    $method = $_GET['method'];
+
+    if ($method == 'atualizarTotal') {
+        // Chame a função para calcular o total e retorne a resposta
+        echo calcularTotalCarrinho();
+    }
+    // Outros métodos podem ser adicionados da mesma forma
+}
+
+
+
+if (isset($_GET['method']) && $_GET['method'] == 'insertIntoCarrinho') {
+    // Verifique e sanitize os parâmetros necessários, e em seguida, chame a função desejada
+    if (isset($_GET['id_produto'])) {
+        $id_produto = $_GET['id_produto'];
+        insertIntoCarrinho($id_produto, $_SESSION["user_id"], 1);
+        // Pode adicionar lógica de resposta aqui, se necessário
+    }
+    // Outros métodos podem ser adicionados da mesma forma
+}
+if (isset($_GET['method']) && $_GET['method'] == 'removeFromCarrinho') {
+    // Verifique e sanitize os parâmetros necessários, e em seguida, chame a função desejada
+    if (isset($_GET['id_produto'])) {
+        $id_produto = $_GET['id_produto'];
+        removeFromCarrinho($id_produto);
+        // Pode adicionar lógica de resposta aqui, se necessário
+    }
+    // Outros métodos podem ser adicionados da mesma forma
+}
+
+if (isset($_GET['action']) && $_GET['action'] == 'atualizar') {
+    // Inclua a função contarItensNoCarrinho aqui (ou o código relevante para atualizar a consulta)
+    
+
+    // Exemplo: $id_cliente = obterIdDoCliente(); // Substitua com sua lógica para obter o ID do cliente
+    $id_cliente = $_SESSION['user_id']; // Substitua pelo ID do cliente atual
+
+    // Chama a função para contar a quantidade de itens no carrinho
+    $quantidadeItens = contarItensNoCarrinho($id_cliente);
+
+    // Retorna a quantidade como resposta AJAX
+    echo $quantidadeItens;
+} else {
+    // Código adicional caso necessário
+}
+
+
+
+// Função para inserir na tabela Compra
+function inserirCompra($valor) {
+    // Inicie a sessão (se ainda não estiver iniciada)
+    session_start();
+    $conn = connect();
+
+    // Verifique se o usuário está logado
+    if (!isset($_SESSION['user_id'])) {
+        die("Usuário não autenticado.");
+    }
+
+    // Obtém o ID do usuário da sessão
+    $usuario_id = $_SESSION['user_id'];
+
+    // Obtém a data e hora atual
+    $dataehora = date('Y-m-d H:i:s');
+
+    // Prepara a instrução SQL para inserção
+    $sql = "INSERT INTO Compra (usuario_id, dataehora, valor) VALUES (?, ?, ?)";
+
+    // Prepara a instrução SQL
+    $stmt = $conn->prepare($sql);
+
+    // Vincula os parâmetros
+    $stmt->bind_param('iss', $usuario_id, $dataehora, $valor);
+
+    // Executa a instrução SQL
+    if ($stmt->execute()) {
+        echo "Compra inserida com sucesso.";
+    } else {
+        echo "Erro ao inserir compra: " . $stmt->error;
+    }
+
+    // Fecha a instrução e a conexão
+    $stmt->close();
+    $conn->close();
+}
+function getCompra() {
+    $conn = connect();
+    $sql = "SELECT * FROM Compra";
+    $result = $conn->query($sql);
+
+    $compras = array();
+
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $compras[] = $row;
+        }
+    }
+
+    return $compras;
+}
+
+function getUserName($id_usuario) {
+    $conn = connect();
+    $sql = "SELECT nome FROM Usuario WHERE id = $id_usuario";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        return $row['nome'];
+    } else {
+        return "Usuário não encontrado";
+    }
+}
+
+
+function getFotoPefil($id_usuario) {
+    $conn = connect();
+    $sql = "SELECT foto FROM Usuario WHERE id = $id_usuario";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        return $row['foto'];
+    } else {
+        return "Usuário não encontrado";
+    }
+}
+
+function updateFotoPerfil($id_usuario, $nova_foto) {
+    $conn = connect();
+
+    // Verifica se o usuário existe antes de tentar atualizar
+    $verifica_sql = "SELECT id FROM Usuario WHERE id = $id_usuario";
+    $verifica_result = $conn->query($verifica_sql);
+
+    if ($verifica_result->num_rows > 0) {
+        // Atualiza o caminho da foto na base de dados
+        $update_sql = "UPDATE Usuario SET foto = '$nova_foto' WHERE id = $id_usuario";
+        $conn->query($update_sql);
+
+        return "Foto de perfil atualizada com sucesso.";
+    } else {
+        return "Usuário não encontrado.";
+    }
+}
+
+
+
+
+function contarItensNoCarrinho($id_cliente) {
+    $conn=connect();
+
+    if ($conn->connect_error) {
+        die("Conexão falhou: " . $conn->connect_error);
+    }
+
+    // Consulta SQL usando DISTINCT para contar IDs únicos no carrinho
+    $sql = "SELECT COUNT(DISTINCT id_produto) AS quantidade FROM carrinho WHERE id_cliente = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("i", $id_cliente);
+
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $quantidade = $row['quantidade'];
+        return $quantidade;
+    } else {
+        return 0;
+    }
+
+    $stmt->close();
+    $conn->close();
+}
+
+
+
 ?>
---
+++ consultas/pitchau.sql
@@ -0,0 +1,52 @@
+CREATE DATABASE IF NOT EXISTS Pitchau;
+USE Pitchau;;
+
+CREATE TABLE IF NOT EXISTS `Categoria` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE IF NOT EXISTS `Compra` (
+  `id_compra` int(11) NOT NULL AUTO_INCREMENT,
+  `usuario_id` int(11) DEFAULT NULL,
+  `dataehora` datetime DEFAULT NULL,
+  `valor` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`id_compra`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE IF NOT EXISTS `Produto` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `descricao` text DEFAULT NULL,
+  `foto` varchar(255) DEFAULT NULL,
+  `valor` decimal(10,2) NOT NULL,
+  `categoria_id` int(11) NOT NULL,
+  `quantidade_estoque` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `categoria_id` (`categoria_id`),
+  CONSTRAINT `Produto_ibfk_1` FOREIGN KEY (`categoria_id`) REFERENCES `Categoria` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE IF NOT EXISTS `Slider` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `url_img` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE IF NOT EXISTS `Usuario` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `isAdmin` tinyint(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=496 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE IF NOT EXISTS `carrinho` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_produto` int(11) DEFAULT NULL,
+  `id_cliente` int(11) DEFAULT NULL,
+  `quantidade` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=169 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
