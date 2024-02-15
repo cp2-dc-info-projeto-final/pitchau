28fd9f4436111f34ae0e3fc5d8cefdfe40a96e80
Athosespinner9 <95544072+ATHOSESPINNER9@users.noreply.github.com>
2024-02-14 21:57:21 -0300

    editprod

--- exibir_produto.php
+++ exibir_produto.php
@@ -701,8 +701,7 @@ $card_produto = recuperar_produto_por_id($id_produto);
     echo '</div>';
     echo '<div class="card-footer">';
     echo '<span class="text-title">$' . number_format($card_produto["valor"], 2) . '</span>';
-    echo '<div class="card-button" id="addcart">';
-    
+    echo '<div class="card-button">';
     // Terminar Depois - Editar Produto
 //      echo '<a href="altera_produto.php?id_produto='.$card_produto["id"].'">Alterar Produto</a>';
 
@@ -718,33 +717,40 @@ $card_produto = recuperar_produto_por_id($id_produto);
   }
 
 ?>
-
+<!-- --------------------------------------- -->
 <form class="form" action="../php/processar_altera_produto.php" method="post" enctype="multipart/form-data">
   <div class="container"> 
     <p class="title">Alterar Produto</p>
     <p>Redefinição de Produto</p>
-  
-    <div class="header"> 
-      <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> 
-      <path d="M7 10V9C7 6.23858 9.23858 4 12 4C14.7614 4 17 6.23858 17 9V10C19.2091 10 21 11.7909 21 14C21 15.4806 20.1956 16.8084 19 17.5M7 10C4.79086 10 3 11.7909 3 14C3 15.4806 3.8044 16.8084 5 17.5M7 10C7.43285 10 7.84965 10.0688 8.24006 10.1959M12 12V21M12 12L15 15M12 12L9 15" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg> <p>Browse File to upload!</p>
-    </div> 
-    <label for="file" class="footer"> 
-      <svg fill="#000000" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M15.331 6H8.5v20h15V14.154h-8.169z"></path><path d="M18.153 6h-.009v5.342H23.5v-.002z"></path></g></svg> 
-      <p>Selecione a foto do Produto</p> 
-      <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M5.16565 10.1534C5.07629 8.99181 5.99473 8 7.15975 8H16.8402C18.0053 8 18.9237 8.9918 18.8344 10.1534L18.142 19.1534C18.0619 20.1954 17.193 21 16.1479 21H7.85206C6.80699 21 5.93811 20.1954 5.85795 19.1534L5.16565 10.1534Z" stroke="#000000" stroke-width="2"></path> <path d="M19.5 5H4.5" stroke="#000000" stroke-width="2" stroke-linecap="round"></path> <path d="M10 3C10 2.44772 10.4477 2 11 2H13C13.5523 2 14 2.44772 14 3V5H10V3Z" stroke="#000000" stroke-width="2"></path> </g></svg>
-    </label> 
-    
-    <label for="foto" class="footer">
-          <!-- ... (seu código SVG) ... -->
-          <p>Selecione a foto do Produto</p>
-          <!-- ... (seu código SVG) ... -->
-        </label>
-        <input id="foto" type="file" name="foto" onchange="previewImage(this)">
-        
-      </div>
-      <p>Obs.: Use dimensões: 163px x 63px<br>para melhor qualidade.</p>
-    <input id="file" type="file" name="foto"> 
-  </div>
+      <div class="header" id="imagePreviewContainer">
+        <img src="../img/upload.png" alt="Imagem padrão" style="width:250px;height:200px">
+        <script>
+          function previewImage(input) {
+            var imagePreviewContainer = document.getElementById('imagePreviewContainer');
+            var file = input.files[0];
+            if (file) {
+              var reader = new FileReader();
+              reader.onload = function (e) {
+                imagePreviewContainer.innerHTML = '<img src="' + e.target.result + '" alt="Imagem a ser carregada" style="width:300px;height:200px;">';
+              };
+              reader.readAsDataURL(file);
+            } 
+            else {
+              // Se nenhum arquivo foi selecionado, exibe a imagem padrão
+              imagePreviewContainer.innerHTML = '<img src="../img/upload.png" alt="Imagem padrão" style="width:250px; height:200px;">';
+            }
+          }
+        </script>
+      </div> 
+      <label for="foto" class="footer">
+        <!-- ... (seu código SVG) ... -->
+        <p>Selecione a foto do Produto</p>
+        <!-- ... (seu código SVG) ... -->
+      </label>
+      <input id="foto" type="file" name="foto" onchange="previewImage(this)">
+      
+    </div>
+    <p>Obs.: Use dimensões: 163px x 63px<br>para melhor qualidade.</p>
   <p class="form-title">Alterar o produto</p>
   <div class="input-container">
     <input type="text" name="nome" placeholder="Nome">
@@ -768,7 +774,7 @@ $card_produto = recuperar_produto_por_id($id_produto);
 	//add_cart = document.getElementById("id").innerHTML = menu;
 	document.body.innerHTML += "<input type='hidden' id='menulevel' value='2'/>";
 </script>
-
+<!-- --------------------------------------- -->
 </div>
 <form class="form" style="margin:auto;" action="" method="post" enctype="multipart/form-data">
   <!-- ... (seu formulário) ... -->
