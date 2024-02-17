60d3bbfa1d61d2f7fd7d94ef13af6e0f16066ed2
Athosespinner9 <95544072+ATHOSESPINNER9@users.noreply.github.com>
2024-02-17 01:54:46 -0300

    +

--- index.php
+++ index.php
@@ -1053,7 +1053,7 @@ atualizarNotificacaoCarrinho()
   </div>
   <div class="fixed-footer">
     <div class="col" id="totalCompraMaster total"style="margin:auto;padding-rigth:10%; font-size: 26px; font-weight: 800;">
-      <span id="total"></span>
+      <span id="total" style="font-color:black"></span>
       <script>
         function atualizarTotal() {
           var xmlhttp_ = new XMLHttpRequest();
