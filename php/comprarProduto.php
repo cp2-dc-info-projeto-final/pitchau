3c0c883d286c2f6b619745a1c7b99240913dd0a8
kaykyCarrilho <galegobar785@gmail.com>
2024-02-17 00:39:25 -0300

    carrinho

--- php/comprarProduto.php
+++ php/comprarProduto.php
@@ -6,6 +6,7 @@
 
     $conn= connect(); 
     $id_cliente= $_SESSION['user_id'];
+    $dataAtual = date('d/m/Y');
     
     
     $produtos_no_carrinho = array(); // Inicializa um array para armazenar os produtos no carrinho
@@ -17,7 +18,6 @@
     if ($res_nome && $res_nome->num_rows > 0) {
         $row = $res_nome->fetch_assoc();
         $nome_usuario = $row['nome'];
-        echo $nome_usuario;
     } 
 
 
@@ -28,7 +28,6 @@
         while ($produto = $res->fetch_assoc()) {
             $id_produto = $produto['produto_id'];
             $quantidade= $produto['quantidade'];
-            $nome_produto= $produto['nome'];
 
             $sql_produto = "SELECT * FROM produto WHERE id = '$id_produto'";
             $res_produto = $conn->query($sql_produto);
@@ -36,14 +35,28 @@
             if ($res_produto) {
                 while ($row_produto = $res_produto->fetch_assoc()) {
                     $produtos_no_carrinho[] = $row_produto; // Adiciona o produto ao array de produtos no carrinho
-                    $sql_inserir= "INSERT INTO historico (usuario_id, usuario_nome, produto_nome, data_compra) VALUES ('$id_cliente', '$nome_usuario ', '$nome_produto') "
+                    $nome_produto= $row_produto['nome'];
+                    $sql_inserir= "INSERT INTO historico (usuario_id, usuario_nome, produto_nome, data_compra) VALUES ('$id_cliente', '$nome_usuario ', '$nome_produto', '$dataAtual') ";
+                    mysqli_query($conn,$sql_inserir);
+                    
+
+
+                    $sql_delete = "DELETE FROM produtocarrinho WHERE usuario_id = '$id_cliente'"; 
+                    mysqli_query($conn,$sql_delete);
+
+
+
 
                         
                 }
             } 
         }
     } 
+
+    header("Location: ../index.php");
+
     
+
 
 
 
--- php/inserir_produto.php
+++ php/inserir_produto.php
@@ -9,5 +9,9 @@ $id_usuario= $_SESSION['user_id'];
 $sql = "INSERT INTO produtocarrinho (usuario_id, produto_id)";
 $sql .= "VALUES ('$id_usuario','$id_produto')";  
 mysqli_query($conn,$sql);
+
+
+
+header("Location: ../index.php");
   
 ?>
