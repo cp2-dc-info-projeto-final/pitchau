304b1df10cf078ac4296de9fae4732e99222a11a
IgorHonorio <141865985+IgorHonorio@users.noreply.github.com>
2024-03-11 13:00:48 -0300

    Add files via upload

--- php/enviar_email.php
+++ php/enviar_email.php
@@ -22,33 +22,24 @@ function envia_email($para, $assunto, $mensagem){
 
     try {
         //Configurações do servidor
-        // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                   //Habilita a saída de debug (para fase de testes)
+        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                   //Habilita a saída de debug (para fase de testes)
         $mail->isSMTP();                                            //Define o envio por meio do SMTP
         $mail->Host       = 'smtp.gmail.com';                       //Define o servidor SMTP utilizado para o envio
         $mail->SMTPAuth   = true;                                   //Habilita a autenticação do SMTP
-        $mail->Username   = 'pitchau0@gmail.com';               //usuário SMTP
-        $mail->Password   = 'admuser123';                     //senha SMTP
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;       //Habilita a encriptação implícita TLS
+        $mail->Username   = 'pitchau0@gmail.com';                   //usuário SMTP
+        $mail->Password   = 'admuser123';                           //senha SMTP
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Habilita a encriptação implícita TLS
         $mail->Port       = 465;                                    //Porta TCP de conexão; use 587 se você tiver configurado SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS
         
 
         //Remetente e Destinatários
-        $mail->setFrom('clinicamanoelgomes@gmail.com', 'Clinica Manoel Gomes');  // Adiciona o remetente
-        $mail->addAddress($para);                                       // Adiciona um destinatário
-        // $mail->addAddress('ellen@example.com');                      // O nome é opcional
-        // $mail->addReplyTo('info@example.com', 'Information');        // Adicona um endereço de resposta
-        // $mail->addCC('cc@example.com');                              // Adiciona um e-mail de cópia
-        // $mail->addBCC('bcc@example.com');                            // Adicona um e-mail de cópia oculta.
-
-        //Anexos
-        // $mail->addAttachment('/var/tmp/file.tar.gz');        //Adiciona um anexo
-        // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');   //O nome é opcional
+        $mail->setFrom('pitchau0@gmail.com', 'Pitchau inc');// Adiciona o remetente
+        $mail->addAddress($para);                           // Adiciona um destinatário
 
         //Conteúdo
         $mail->isHTML(true);                                    //Formata o e-mail com HTML
-        $mail->Subject = $assunto;                              //Assunto do e-mail
-        $mail->Body    = $mensagem;                             // Corpo do e-mail
-        // $mail->AltBody = 'Texto sem tags HTML!';             //Opção de texto para provedores de e-mail que não lêem HTML.
+        $mail->Subject = $assunto;                            //Assunto do e-mail
+        $mail->Body = $mensagem;                                // Corpo do e-mail
 
         $mail->send();                                          // tenta enviar o e-mail
         return true;                                            // retorna verdadeiro se enviar corretamente.
--- php/enviar_mensagem.php
+++ php/enviar_mensagem.php
@@ -1,40 +1,41 @@
 <?php
 include_once "../consultas/flying_bubbles.php";
-$conn= connect();
-$email= $_REQUEST['email'];
-$sql= "SELECT * FROM usuario WHERE email = '$email'";
-$res= mysqli_query($conn,$sql);
-$linhas= mysqli_num_rows($res);
+include_once "enviar_email.php";
 
-$resultado = mysqli_fetch_array ($res);
+$conn = connect();
 
-$code = rand(100000,999999);
+if (isset($_REQUEST['email'])) {
+    $email = $_REQUEST['email'];
 
-if($resultado["email"] == $email){
-    include "enviar_email.php";
+    $sql = "SELECT * FROM usuario WHERE email = '$email'";
+    $res = mysqli_query($conn, $sql);
+    $linhas = mysqli_num_rows($res);
+
+    if ($linhas > 0) {
+        $resultado = mysqli_fetch_array($res);
+
+        $code = rand(100000, 999999);
+
+        $para = $resultado['$email' ];
+        $assunto = "Recuperação de Senha";
+        $mensagem = "Sua nova senha é: $code";
+
+        // Chama a função envia_email
+        envia_email($para, $assunto, $mensagem);
+
+        $senha_cript = password_hash($code, PASSWORD_DEFAULT);
     
+        $sql_senha = "UPDATE usuario SET senha = '$senha_cript' WHERE email = '$email'";  
+        mysqli_query($conn, $sql_senha);
 
-    $para = $resultado["email"];
-    $assunto = "Recuperação de Senha";
+        $_SESSION['msg'] = "<div class='alert alert-success' role='alert'>Enviamos uma nova senha para o seu email</div>";
+        header("Location: ../paginas/esqueceu_senha.php");
+    } else {
+        $_SESSION['msg'] = "<div class='alert alert-danger'>Email não encontrado</div>";
+        header("Location: ../paginas/esqueceu_senha.php");
+        exit();
+    }
 
-    $mensagem = "Sua nova senha é: $code";
-
-    envia_email($para, $assunto, $mensagem);
-
-
-    $senha_cript = password_hash($code, PASSWORD_DEFAULT);
-    
-    $sql_senha = "UPDATE usuario SET senha = '$senha_cript' WHERE email = '$email'";  
-    mysqli_query($conn,$sql_senha);
-
-    $_SESSION['msg'] = "<div class='alert alert-success' role='alert'>Enviamos uma nova senha para o seu email</div>";
-    //header("Location: ../paginas/esqueceu_senha.php");
-
+    echo $email;
 }
-else{
-    $_SESSION['msg'] = "<div class='alert alert-danger'>Email não encontrado</div>";
-    //header("Location: ../paginas/esqueceu_senha.php");
-    exit();
-}
-echo $email;
 ?>
